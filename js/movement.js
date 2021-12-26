ea1dcc3c4388e7a75bb273942c016fa8058c2380
Franweb79 <franprietogutweb@gmail.com>
2021-12-27 00:55:57 +0100

    added some todos

--- js/movement.js
+++ js/movement.js
@@ -2,6 +2,8 @@
 
 //TODO seems when canvas moves to left, bar disappears a bit, look why
 
+//todo do with modules, why cant properly do global variables with variables.js and why it says movement.js didnt loaded beacuse of mime but it works
+
 let moveWallToLeft=()=>{
     setInterval(() => {
         if(myCanvas.width>300){
