85868fdb084fe49207bfac86d943017b41a945db
Franweb79 <franprietogutweb@gmail.com>
2021-12-27 08:29:04 +0100

    wall working, edit image

--- js/variables.js
+++ js/variables.js
@@ -6,10 +6,28 @@ pa q lo puedan usar todos los modulos
 //Y POR ENCIMA DEL BODY, Y TRATARLO DE MANERA DINAMICA SU WIDTH Y BACKGROUNDSIZE CON JAVASCRIPT
 const myCanvas = document.getElementById('myCanvas');
 
+const myBackground = document.getElementById('myBackground');
 
 const ctx = myCanvas.getContext('2d');
 
+//TODO mira bien esta mierda del innerwidth diferencias con width a secas
 
-//will set width to half canvas to avoid stress moving the bar
-const width = myCanvas.width = window.innerWidth;
-const height = myCanvas.height = window.innerHeight;
+myCanvas.width = window.innerWidth;
+ myCanvas.height  = window.innerHeight;
+
+ //TODO apunta que aqui hace falta .style.width y con el canvas no, y que tuve que poner absolute
+ //TODO recorta la foto para que la L no haya espacios con la pared del canvas
+myBackground.style.width=(window.innerWidth).toString()+"px";
+myBackground.style.height=window.innerHeight.toString()+"px";
+myBackground.style.backgroundImage= "url('../assets/img/limits.png')";
+myBackground.style.backgroundSize= "80% 100%";
+myBackground.style.backgroundRepeat="no-repeat";
+myBackground.style.display="inline-block";
+myBackground.style.position="absolute";
+
+
+
+
+console.log (myCanvas.width);
+console.log (myBackground.width);
+console.log (window.innerHeight);
